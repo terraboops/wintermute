3ab842c26fd8aaa3873f3de8dfa7b7a240ecdaa7
Tyler Mauthe <tmauthe@hatsize.com>
2015-12-07 01:12:32 -0700

    Add Puppet and fix ssh pw

--- puppet/base.pp
+++ puppet/base.pp
@@ -2,21 +2,26 @@ class wintermute {
 	require ::apt
 
 	# Xonotic - playdeb repo
-	apt::key { 'playdeb':
-		source => http://archive.getdeb.net/getdeb-archive.key,
-	}
-
 	apt::source { 'playdeb':
 		location => 'http://archive.getdeb.net/ubuntu',
 		release  => 'wily-getdeb',
-		repos    => 'main',
-		require  => Apt::Key['playdeb'],
+		repos    => 'games',
+		key      => {
+			id     => '1958A549614CE21CFC27F4BAA8A515F046D7E7CF',
+			source => 'http://archive.getdeb.net/getdeb-archive.key',
+		}
 	}
-
 	package { 'xonotic':
-		ensure => 'latest'
+		ensure  => 'latest',
+		require => Apt::Source['playdeb']
 	}
 
+	# Install Steam - HLDS
 	include steam::hlds
+	# Install Steam - steamcmd
 	include steam::steamcmd
+	# Install Open VM Tools
+	include ::openvmtools
 }
+
+include wintermute
