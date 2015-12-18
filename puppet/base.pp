c30cee784fb28e95ede03e2e8eecb27360b867b0
Tyler Mauthe <tmauthe@hatsize.com>
2015-12-17 18:20:38 -0700

    Finalise Xonotic installation

--- puppet/base.pp
+++ puppet/base.pp
@@ -1,23 +1,8 @@
 class wintermute {
-	require ::apt
-
-	# Xonotic - playdeb repo
-	apt::source { 'playdeb':
-		location => 'http://archive.getdeb.net/ubuntu',
-		release  => 'wily-getdeb',
-		repos    => 'games',
-		key      => {
-			id     => '1958A549614CE21CFC27F4BAA8A515F046D7E7CF',
-			source => 'http://archive.getdeb.net/getdeb-archive.key',
-		}
-	}
-	Apt::Source['playdeb'] -> Package<|title == 'xonotic'|>
-	package { 'xonotic':
-		ensure  => 'latest'
-	}
-
+	# Install Xonotic
+	include ::xonotic
 	# Install Steam - steamcmd
-	include steam::steamcmd
+	include ::steam::steamcmd
 	# Install Open VM Tools
 	include ::openvmtools
 }
